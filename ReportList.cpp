f74372b124bb618762858b056a59a55618d641b5
hinagesi_708 <hinagesi-708@users.noreply.github.com>
2015-08-25 23:59:10 +0900

    Update ReportList.cpp

--- ReportList.cpp
+++ ReportList.cpp
@@ -28,4 +28,41 @@ String^ ReportItem::ToString() {
 
 ReportList::ReportList()
 {
+	this->items = gcnew List<ReportItem^>;
+}
+
+//リストのクリア
+void ReportList::Clear() {
+	this->items->Clear();
+}
+//リストに追加
+void ReportList::Add(DateTime date, String^ name, String^ kind, String^ memo) {
+	ReportItem^ item = gcnew ReportItem(date, name, kind, memo);
+	this->Add(item);
+}
+void ReportList::Add(ReportItem^ item) {
+	this->items->Add(item);
+}
+//リストから削除
+void ReportList::Remove(DateTime date, String^ name, String^ memo) {
+	for each (ReportItem^ item in this->items) {
+		if (item->Date == date && item->Name == name) {
+			Remove(item);
+			break;
+		}
+	}
+}
+void ReportList::Remove(ReportItem^ item) {
+	this->items->Remove(item);
+}
+//年月で選択
+List<ReportItem^>^ ReportList::Select(int year, int month) {
+	//return nullptr;
+	List<ReportItem^>^ result = gcnew List<ReportItem^>;
+	for each (ReportItem^ item in this->items) {
+		if (item->Date.Year == year && item->Date.Month == month) {
+			result->Add(item);
+		}
+	}
+	return result;
 }
