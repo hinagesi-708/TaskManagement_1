bd7b3c4fc3c94720f480e95d883d8abacd8543ca
hinagesi_708 <hinagesi-708@users.noreply.github.com>
2015-08-25 23:59:56 +0900

    Update MyForm.cpp

--- MyForm.cpp
+++ MyForm.cpp
@@ -3,8 +3,10 @@
 using namespace TaskManagement_1;
 
 [STAThreadAttribute]
-int main() {
-	MyForm ^fm = gcnew MyForm();
-	fm->ShowDialog();
+int main(array<System::String ^> ^args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Application::Run(gcnew MyForm());
 	return 0;
 }
